3ab3ed4db9c58ed32dad3cda5f2770683833c2dc
Sarah Bird <sarah@bonvaya.com>
2015-12-13 22:05:26 -0800

    Add dashboard layouts

--- happiness/happiness/views.py
+++ happiness/happiness/views.py
@@ -1,26 +1,41 @@
 from django.contrib.auth.models import User
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 
 from .models import Team
 
 
-class BaseView(TemplateView):
+class ContextMixin(object):
 
     def get_context_data(self, *args, **kwargs):
-        context = super(BaseView, self).get_context_data(*args, **kwargs)
+        context = super(ContextMixin, self).get_context_data(*args, **kwargs)
         users = User.objects.filter(is_superuser=False).order_by('username')
         teams = Team.objects.all().order_by('name')
-        context.update(users=users, teams=teams)
+        context.update(all_users=users, all_teams=teams)
         return context
 
 
-class HomeView(BaseView):
+class HomeView(ContextMixin, TemplateView):
     template_name = 'happiness/home.html'
 
 
-class IndividualDashboardView(BaseView):
-    template_name = 'happiness/home.html'
+class IndividualDashboardView(ContextMixin, DetailView):
+    template_name = 'happiness/dashboard_individual.html'
+    model = User
+    context_object_name = 'user'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(IndividualDashboardView, self).get_context_data(*args, **kwargs)
+        context.update(dashboard='individual')
+        return context
 
 
-class TeamDashboardView(BaseView):
-    template_name = 'happiness/home.html'
+class TeamDashboardView(ContextMixin, DetailView):
+    template_name = 'happiness/dashboard_team.html'
+    model = User
+    context_object_name = 'user'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TeamDashboardView, self).get_context_data(*args, **kwargs)
+        context.update(dashboard='team')
+        return context
