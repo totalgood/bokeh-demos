ab3f9d4635018bf74fe16af83151cdf502cfa78e
Sarah Bird <sarah@bonvaya.com>
2015-12-14 18:57:00 -0800

    Fix disappearing plots
    Do not add source to document root

--- happiness/viz/individual.py
+++ happiness/viz/individual.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from bokeh.io import curdoc
 from bokeh.models import (
     ColumnDataSource,
@@ -12,8 +12,8 @@ from bokeh.models import (
 
 document = curdoc()
 
-start_date = datetime(2015, 6, 1)
-end_date = datetime(2015, 8, 31)
+start_date = date(2015, 6, 1)
+end_date = date(2015, 8, 31)
 
 x_range = Range1d(start_date, end_date)
 y_range = Range1d(0, 10)
@@ -23,14 +23,10 @@ plot = Plot(
     x_range=x_range, y_range=y_range,
     outline_line_color=None,
 )
-
 plot.add_layout(LinearAxis(), 'left')
 plot.add_layout(DatetimeAxis(axis_label=None), 'below')
 
 source = ColumnDataSource(data=dict(x=[], y=[]), name='source')
-
-# This will generate an error, but its ok, as we add data to the session
 plot.add_glyph(source, Line(x='x', y='y', line_width=3, line_alpha=0.6, line_color='magenta'))
 
-document.add_root(source)
 document.add_root(plot)
