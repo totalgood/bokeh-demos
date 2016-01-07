0e821b298415b5f4fe081c216cfd291a81a5883a
Sarah Bird <sarah@bonvaya.com>
2016-01-07 13:03:53 -0500

    Lots of small cleanup
    - de-dupe some server code
    - tweak frequency of callbacks
    - improve rendering of legends

--- happiness/viz/teams.py
+++ happiness/viz/teams.py
@@ -1,11 +1,13 @@
 from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource
 from bokeh.palettes import Spectral4
-from django.core.exceptions import AppRegistryNotReady
-from django.contrib.auth.models import User
 
-from viz.utils import make_plot, setup_django
+from viz.utils import make_plot, get_user, django_setup
 
+if not django_setup:
+    import django
+    django.setup()
+    django_setup = True
 document = curdoc()
 
 user_pk_source = ColumnDataSource(data=dict(user_pk=[]), name='user_pk_source')
@@ -20,23 +22,19 @@ plot.line(x='x', y='y', line_width=2, line_color=Spectral4[3], line_cap='round',
 
 
 def update_data():
-    user_pk = document.get_model_by_name('user_pk_source').data['user_pk'][0]
-    try:
-        team = User.objects.get(pk=user_pk).team
+    user = get_user(document)
+    if user and hasattr(user, 'team'):
+        team = user.team
         dates, happiness = team.get_team_dates_happiness()
         new_data = dict(x=dates, y=happiness)
         source.data = new_data
-
-    except User.DoesNotExist:
-        pass
-    except AppRegistryNotReady:
-        setup_django()
 
 
 def update_data_once():
     update_data()
 
+
 document.add_root(plot)
 document.add_root(user_pk_source)
-document.add_timeout_callback(update_data_once, 100)
-document.add_periodic_callback(update_data, 2000)
+document.add_timeout_callback(update_data_once, 250)
+document.add_periodic_callback(update_data, 5000)
--- happiness/viz/utils.py
+++ happiness/viz/utils.py
@@ -1,6 +1,9 @@
 from bokeh.plotting import Figure
-from bokeh.models import Range1d, Legend
+from bokeh.models import Range1d, Legend, FixedTicker
+from django.core.exceptions import AppRegistryNotReady
+from django.contrib.auth.models import User
 
+django_setup = False
 
 def make_plot():
     plot = Figure(
@@ -8,12 +11,22 @@ def make_plot():
         plot_width=800,
         responsive=True,
         tools="xpan,xwheel_zoom,xbox_zoom,reset",
-        x_axis_type='datetime'
+        x_axis_type='datetime',
+        min_border_top=10,
+        min_border_right=0,
+        min_border_bottom=0,
+        min_border_left=30,
+        outline_line_color=None,
     )
     plot.x_range.follow = "end"
     plot.x_range.follow_interval = 120 * 24 * 60 * 60 * 1000
     plot.x_range.range_padding = 0
     plot.y_range = Range1d(0, 12)
+    plot.xgrid.grid_line_color = None
+    plot.ygrid.grid_line_color = None
+    plot.yaxis.bounds = (0, 9)
+    plot.yaxis.minor_tick_line_color = None
+    plot.yaxis.ticker = FixedTicker(ticks=[0, 3, 6, 9])
     return plot
 
 
@@ -26,7 +39,12 @@ def make_legend():
     )
 
 
-def setup_django():
-    print('Setting up django')
-    import django
-    django.setup()
+def get_user(document):
+    try:
+        user_pk = document.get_model_by_name('user_pk_source').data['user_pk'][0]
+        user = User.objects.get(pk=user_pk)
+        return user
+    except IndexError:
+        return None
+    except User.DoesNotExist:
+        return None
